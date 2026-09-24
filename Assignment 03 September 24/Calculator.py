518109695d1f694a493facd40cfb698d55f4ef4b
Kyrylo <kirillborisenko758us@gmail.com>
2026-09-24 16:54:21 -0500

    Done with a new function 'try' and 'except'

---
+++ Assignment 03 September 24/Calculator.py
@@ -0,0 +1,29 @@
+operation = input("What do you want +, -, *, /: ")
+
+if operation == "+":
+    firstNum = int(input("First number: "))
+    secondNum = int(input("Second number: "))
+    result = firstNum + secondNum
+    print("result: " + str(result))
+
+if operation == "-":
+    firstNum = int(input("First number: "))
+    secondNum = int(input("Second number: "))
+    result = firstNum - secondNum
+    print("result: " + str(result))
+
+if operation == "*":
+    firstNum = int(input("First number: "))
+    secondNum = int(input("Second number: "))
+    result = firstNum * secondNum
+    print("result: " + str(result))
+
+if operation == "/":
+    try:
+        firstNum = int(input("First number: "))
+        secondNum = int(input("Second number: "))
+        result = firstNum / secondNum
+    except ZeroDivisionError:
+        print("You can't divide by 0")
+    else:
+        print("result: " + str(result))
